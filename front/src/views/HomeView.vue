210e8ff030f2fb0c2ec95d2ab8d1ccb61e4066a2
Stephane Andriamihaja <stephane.andriamihaja@metroscope.tech>
2025-02-01 00:51:50 +0100

    chore(front): Installing and setting up frontend (using vue)

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue';
+import * as countries from '../data/countries.json';
+
+console.log(countries, 'toto');
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
